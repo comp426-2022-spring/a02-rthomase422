d881ec7b789575700205024eb8a89a9add5da492
rthomase422 <rthomase@live.unc.edu>
2022-03-20 15:14:15 -0400

    flips 2

--- flips.js
+++ flips.js
@@ -1,7 +1,11 @@
-const args = require('yargs').argv;
-console.log(args.number);
-const number = args.number || 1
+//const args = require('yargs').argv;
+//console.log(args.number);
+//const number = args.number || 1
 //if no number given, flip once by default
+import Yargs from 'yargs';
+const args = Yargs(process.argv.slice(2)).argv;
+const number = args.number || process.env.NUMBER || 1
+console.log(args.number)
 import { coinFlips, countFlips } from './modules/coin.mjs';
 console.log(coinFlips(number))
 console.log(countFlips())

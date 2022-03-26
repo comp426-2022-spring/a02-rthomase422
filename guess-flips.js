edd979ff36eefb87680422c368b737a229b20914
rthomase422 <rthomase@live.unc.edu>
2022-03-26 13:21:30 -0400

    guess-flips

--- guess-flips.js
+++ guess-flips.js
@@ -1,3 +1,16 @@
-import argv from 'yargs';
-console.log(argv.call)
+//import argv from 'yargs';
+//console.log(argv.call)
 import { flipACoin } from './modules/coin.mjs';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+var argv = require('yargs')(process.argv.slice(2))
+    //.scriptName("guess-flips")
+    .usage("node guess-flip --call=[heads|tails]")
+    .alias('c', 'call')
+    .choices('c', ['heads', 'tails'])
+    .strict()
+    .argv
+;
+const call = argv.call || process.env.CALL
+
+console.log(flipACoin(call))

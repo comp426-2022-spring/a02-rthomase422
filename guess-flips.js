ae6df63d8a48d5f77e8a39506e3bc2c5cb1fd202
rthomase422 <rthomase@live.unc.edu>
2022-04-21 17:04:17 -0400

    guess_flips

--- guess-flips.js
+++ guess-flips.js
@@ -1,16 +1,11 @@
 //import argv from 'yargs';
 //console.log(argv.call)
 import { flipACoin } from './modules/coin.mjs';
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const args = require('yargs')(process.argv.slice(2));
-//args['call']
-const call = args.call;
-if (call == null) {
-    console.log("Error: no input.")
-    //console.log("Usage: node guess-flip --call=[heads|tails]")
-} else if (call != "heads" && call != "tails") {
-    console.log("Error: not a valid input (heads or tails).")
+import minimist from 'minimist';
+var args = minimist(process.argv.slice(2));
+let call = flipACoin(args.call);
+if (call != "heads" && call != "tails") {
+    console.log("Error: no inputs\nUsage: node guess-flip --call=[heads|tails]")
     //console.log("Usage: node guess-flip --call=[heads|tails]")
 } else {
     console.log(flipACoin(call))

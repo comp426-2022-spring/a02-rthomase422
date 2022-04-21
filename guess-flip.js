930f80e8817db4194dcfe0c4b5d7f8d0cd64c621
rthomase422 <rthomase@live.unc.edu>
2022-04-21 17:18:22 -0400

    i need coffee

--- guess-flip.js
+++ guess-flip.js
@@ -4,10 +4,9 @@ import { flipACoin } from './modules/coin.mjs';
 import minimist from 'minimist';
 var args = minimist(process.argv.slice(2));
 let call = args['call']
-if (call != "heads" && call != "tails") {
-    console.log("Error: no inputs\nUsage: node guess-flip --call=[heads|tails]")
-    //console.log("Usage: node guess-flip --call=[heads|tails]")
-} else {
+if (call == "heads" || call == "tails") {
     console.log(flipACoin(call))
+} else {
+    console.log("Error: no inputs\nUsage: node guess-flip --call=[heads|tails]")
 }
 

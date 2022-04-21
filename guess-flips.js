a0b22383c0d8af38df1c3492f3f9d1aaf91945c8
rthomase422 <rthomase@live.unc.edu>
2022-04-21 17:06:47 -0400

    maybe

--- guess-flips.js
+++ guess-flips.js
@@ -3,7 +3,7 @@
 import { flipACoin } from './modules/coin.mjs';
 import minimist from 'minimist';
 var args = minimist(process.argv.slice(2));
-let call = flipACoin(args.call);
+let call = args['call']
 if (call != "heads" && call != "tails") {
     console.log("Error: no inputs\nUsage: node guess-flip --call=[heads|tails]")
     //console.log("Usage: node guess-flip --call=[heads|tails]")
